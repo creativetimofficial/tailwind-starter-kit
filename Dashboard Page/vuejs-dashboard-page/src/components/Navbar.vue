f9e62510569dfcfaa930f018b5acfabfe6220ed7
jerzy-dudzic <30810342+jerzy-dudzic@users.noreply.github.com>
2019-12-31 07:18:51 +0100

    typo in css class name in Navbar.vue
    Typo

--- Dashboard Page/vuejs-dashboard-page/src/components/Navbar.vue
+++ Dashboard Page/vuejs-dashboard-page/src/components/Navbar.vue
@@ -4,7 +4,7 @@
     class="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-no-wrap md:justify-start flex items-center p-4"
   >
     <div
-      class="w-full mx-autp items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4"
+      class="w-full mx-auto items-center flex justify-between md:flex-no-wrap flex-wrap md:px-10 px-4"
     >
       <!-- Brand -->
       <a
