3db2377e19ea64d111c993cdd7cb4b77cb62a093
EINazare <emanuelioannazare@gmail.com>
2021-03-22 18:21:00 +0200

    wip: in process of update tailwind

--- Dashboard Page/react-dashboard-page/src/components/Sidebar.js
+++ Dashboard Page/react-dashboard-page/src/components/Sidebar.js
@@ -8,8 +8,8 @@ export default function Sidebar() {
   const [collapseShow, setCollapseShow] = React.useState("hidden");
   return (
     <>
-      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-no-wrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
-        <div className="md:flex-col md:items-stretch md:min-h-full md:flex-no-wrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+      <nav className="md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10 py-4 px-6">
+        <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
           {/* Toggler */}
           <button
             className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
@@ -20,7 +20,7 @@ export default function Sidebar() {
           </button>
           {/* Brand */}
           <Link
-            className="md:block text-left md:pb-2 text-gray-700 mr-0 inline-block whitespace-no-wrap text-sm uppercase font-bold p-4 px-0"
+            className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
             to="/"
           >
             Tailwind Starter Kit
@@ -42,11 +42,11 @@ export default function Sidebar() {
             }
           >
             {/* Collapse header */}
-            <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-gray-300">
+            <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
               <div className="flex flex-wrap">
                 <div className="w-6/12">
                   <Link
-                    className="md:block text-left md:pb-2 text-gray-700 mr-0 inline-block whitespace-no-wrap text-sm uppercase font-bold p-4 px-0"
+                    className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                     to="/"
                   >
                     Tailwind Starter Kit
@@ -69,7 +69,7 @@ export default function Sidebar() {
                 <input
                   type="text"
                   placeholder="Search"
-                  className="px-3 py-2 h-12 border border-solid  border-gray-600 placeholder-gray-400 text-gray-700 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
+                  className="px-3 py-2 h-12 border border-solid  border-blueGray-500 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-base leading-snug shadow-none outline-none focus:outline-none w-full font-normal"
                 />
               </div>
             </form>
@@ -86,110 +86,110 @@ export default function Sidebar() {
 
               <li className="items-center">
                 <Link
-                  className="text-gray-800 hover:text-gray-600 text-xs uppercase py-3 font-bold block"
+                  className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
                   to="/"
                 >
-                  <i className="fas fa-newspaper text-gray-500 mr-2 text-sm"></i> Landing Page
+                  <i className="fas fa-newspaper text-blueGray-400 mr-2 text-sm"></i> Landing Page
                 </Link>
               </li>
 
               <li className="items-center">
                 <Link
-                  className="text-gray-800 hover:text-gray-600 text-xs uppercase py-3 font-bold block"
+                  className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
                   to="/"
                 >
-                  <i className="fas fa-user-circle text-gray-500 mr-2 text-sm"></i> Profile Page
+                  <i className="fas fa-user-circle text-blueGray-400 mr-2 text-sm"></i> Profile Page
                 </Link>
               </li>
 
               <li className="items-center">
                 <Link
-                  className="text-gray-800 hover:text-gray-600 text-xs uppercase py-3 font-bold block"
+                  className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
                   to="/"
                 >
-                  <i className="fas fa-fingerprint text-gray-500 mr-2 text-sm"></i> Login
+                  <i className="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i> Login
                 </Link>
               </li>
 
               <li className="items-center">
                 <a
-                  className="text-gray-400 text-xs uppercase py-3 font-bold block"
+                  className="text-blueGray-300 text-xs uppercase py-3 font-bold block"
                   href="#pablo"
                   onClick={e => e.preventDefault()}
                 >
-                  <i className="fas fa-clipboard-list text-gray-400 mr-2 text-sm"></i> Register (soon)
+                  <i className="fas fa-clipboard-list text-blueGray-300 mr-2 text-sm"></i> Register (soon)
                 </a>
               </li>
 
               <li className="items-center">
                 <a
-                  className="text-gray-400 text-xs uppercase py-3 font-bold block"
+                  className="text-blueGray-300 text-xs uppercase py-3 font-bold block"
                   href="#pablo"
                   onClick={e => e.preventDefault()}
                 >
-                  <i className="fas fa-tools text-gray-400 mr-2 text-sm"></i> Settings (soon)
+                  <i className="fas fa-tools text-blueGray-300 mr-2 text-sm"></i> Settings (soon)
                 </a>
               </li>
             </ul>
             {/* Divider */}
             <hr className="my-4 md:min-w-full" />
             {/* Heading */}
-            <h6 className="md:min-w-full text-gray-600 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
+            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
               Documentation
             </h6>
             {/* Navigation */}
             <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
               <li className="inline-flex">
                 <Link
-                  className="text-gray-800 hover:text-gray-600 text-sm block mb-4 no-underline font-semibold"
+                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
                   to="/"
                 >
-                  <i className="fas fa-paint-brush mr-2 text-gray-500 text-base"></i> Styles
+                  <i className="fas fa-paint-brush mr-2 text-blueGray-400 text-base"></i> Styles
                 </Link>
               </li>
 
               <li className="inline-flex">
                 <Link
-                  className="text-gray-800 hover:text-gray-600 text-sm block mb-4 no-underline font-semibold"
+                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
                   to="/"
                 >
-                  <i className="fab fa-css3-alt mr-2 text-gray-500 text-base"></i> CSS Components
+                  <i className="fab fa-css3-alt mr-2 text-blueGray-400 text-base"></i> CSS Components
                 </Link>
               </li>
 
               <li className="inline-flex">
                 <Link
-                  className="text-gray-800 hover:text-gray-600 text-sm block mb-4 no-underline font-semibold"
+                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
                   to="/"
                 >
-                  <i className="fab fa-vuejs mr-2 text-gray-500 text-base"></i> VueJS
+                  <i className="fab fa-vuejs mr-2 text-blueGray-400 text-base"></i> VueJS
                 </Link>
               </li>
 
               <li className="inline-flex">
                 <Link
-                  className="text-gray-800 hover:text-gray-600  text-sm block mb-4 no-underline font-semibold"
+                  className="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold"
                   to="/"
                 >
-                  <i className="fab fa-react mr-2 text-gray-500 text-base"></i> React
+                  <i className="fab fa-react mr-2 text-blueGray-400 text-base"></i> React
                 </Link>
               </li>
 
               <li className="inline-flex">
                 <Link
-                  className="text-gray-800 hover:text-gray-600  text-sm block mb-4 no-underline font-semibold"
+                  className="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold"
                   to="/"
                 >
-                  <i className="fab fa-angular mr-2 text-gray-500 text-base"></i> Angular
+                  <i className="fab fa-angular mr-2 text-blueGray-400 text-base"></i> Angular
                 </Link>
               </li>
 
               <li className="inline-flex">
                 <Link
-                  className="text-gray-800 hover:text-gray-600  text-sm block mb-4 no-underline font-semibold"
+                  className="text-blueGray-700 hover:text-blueGray-500  text-sm block mb-4 no-underline font-semibold"
                   to="/"
                 >
-                  <i className="fab fa-js-square mr-2 text-gray-500 text-base"></i> Javascript
+                  <i className="fab fa-js-square mr-2 text-blueGray-400 text-base"></i> Javascript
                 </Link>
               </li>
             </ul>
