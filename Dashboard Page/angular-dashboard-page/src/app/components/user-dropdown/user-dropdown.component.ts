3cf74c29ff37c3b798cb11f41cd7e00ddf92a48a
EINazare <emanuelioannazare@gmail.com>
2021-03-22 18:20:46 +0200

    wip: in process of update tailwind

--- Dashboard Page/angular-dashboard-page/src/app/components/user-dropdown/user-dropdown.component.ts
+++ Dashboard Page/angular-dashboard-page/src/app/components/user-dropdown/user-dropdown.component.ts
@@ -1,49 +1,30 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import Popper from "popper.js";
+import { Component, AfterViewInit, ViewChild, ElementRef } from "@angular/core";
+import { createPopper } from "@popperjs/core";
 
 @Component({
   selector: 'app-user-dropdown',
   templateUrl: './user-dropdown.component.html'
 })
-export class UserDropdownComponent implements OnInit {
+export class UserDropdownComponent implements AfterViewInit {
   dropdownPopoverShow = false;
-  @ViewChild('btnDropdownRef',{ static: false }) btnDropdownRef:ElementRef;
-  popper = document.createElement("div");
-  ngOnInit() {
-    this.popper.innerHTML = `<div class="bg-white text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1" style="min-width:12rem" #popoverDropdownRef>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Action
-  </a>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Another action
-  </a>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Something else here
-  </a>
-  <div class="h-0 my-2 border border-solid border-gray-200"></div>
-  <a href="#pablo" class="text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent  text-gray-800">
-    Seprated link
-  </a>
-</div>`;
+  @ViewChild("btnDropdownRef", { static: false }) btnDropdownRef: ElementRef;
+  @ViewChild("popoverDropdownRef", { static: false })
+  popoverDropdownRef: ElementRef;
+  ngAfterViewInit() {
+    createPopper(
+      this.btnDropdownRef.nativeElement,
+      this.popoverDropdownRef.nativeElement,
+      {
+        placement: "bottom-start",
+      }
+    );
   }
-  toggleDropdown(event){
+  toggleDropdown(event) {
     event.preventDefault();
-    if(this.dropdownPopoverShow){
+    if (this.dropdownPopoverShow) {
       this.dropdownPopoverShow = false;
-      this.destroyPopper();
     } else {
       this.dropdownPopoverShow = true;
-      this.createPoppper();
     }
-  }
-  destroyPopper(){
-    this.popper.parentNode.removeChild(this.popper);
-  }
-  createPoppper(){
-    new Popper(this.btnDropdownRef.nativeElement, this.popper, {
-      placement: "bottom-end"
-    });
-    this.btnDropdownRef.nativeElement.parentNode.insertBefore(this.popper, this.btnDropdownRef.nativeElement.nextSibling);
-
   }
 }
