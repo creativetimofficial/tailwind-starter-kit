d20ca0c1006344308be2a4ef6d406f5dc5f70d14
EINazare <emanuelioannazare@gmail.com>
2021-03-22 18:20:55 +0200

    wip: in process of update tailwind

--- Dashboard Page/react-dashboard-page/src/components/BarChart.js
+++ Dashboard Page/react-dashboard-page/src/components/BarChart.js
@@ -105,10 +105,10 @@ export default function BarChart() {
           <div className="rounded-t mb-0 px-4 py-3 bg-transparent">
             <div className="flex flex-wrap items-center">
               <div className="relative w-full max-w-full flex-grow flex-1">
-                <h6 className="uppercase text-gray-500 mb-1 text-xs font-semibold">
+                <h6 className="uppercase text-blueGray-400 mb-1 text-xs font-semibold">
                   Performance
                 </h6>
-                <h2 className="text-gray-800 text-xl font-semibold">
+                <h2 className="text-blueGray-700 text-xl font-semibold">
                   Total orders
                 </h2>
               </div>
