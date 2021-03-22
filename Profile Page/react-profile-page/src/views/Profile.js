557bdcde898f2dcb47c367f1b6fb2595701336f1
EINazare <emanuelioannazare@gmail.com>
2021-03-22 18:21:56 +0200

    wip: in process of update tailwind

--- Profile Page/react-profile-page/src/views/Profile.js
+++ Profile Page/react-profile-page/src/views/Profile.js
@@ -50,7 +50,7 @@ export default function Profile() {
                     <div className="relative">
                       <img
                         alt="..."
-                        src={require("assets/img/team-2-800x800.jpg")}
+                        src={require("assets/img/team-2-800x800.jpg").default}
                         className="shadow-xl rounded-full h-auto align-middle border-none absolute -m-16 -ml-20 lg:-ml-16"
                         style={{ maxWidth: "150px" }}
                       />
