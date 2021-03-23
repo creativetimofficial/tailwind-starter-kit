fd79ba0f85757df36be6f405680f5d129a13f396
EINazare <emanuelioannazare@gmail.com>
2021-03-23 12:37:27 +0200

    refactor: update to tailwind v2

--- Dashboard Page/react-dashboard-page/src/components/Navbar.js
+++ Dashboard Page/react-dashboard-page/src/components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar() {
               <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
                 <i className="fas fa-search"></i>
               </span>
-              <input type="text" placeholder="Search here..." className="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
+              <input type="text" placeholder="Search here..." className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm shadow outline-none focus:outline-none focus:ring w-full pl-10"/>
             </div>
           </form>
           {/* User */}
