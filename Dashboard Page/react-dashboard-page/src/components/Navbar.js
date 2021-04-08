3c294f297c881615a55a64db414d74010a675c8e
Dan Walker <github@danwalker.com>
2021-04-08 01:23:02 +0100

    Fix autp typo

--- Dashboard Page/react-dashboard-page/src/components/Navbar.js
+++ Dashboard Page/react-dashboard-page/src/components/Navbar.js
@@ -7,7 +7,7 @@ export default function Navbar() {
     <>
       {/* Navbar */}
       <nav className="absolute top-0 left-0 w-full z-10 bg-transparent md:flex-row md:flex-nowrap md:justify-start flex items-center p-4">
-        <div className="w-full mx-autp items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
+        <div className="w-full mx-auto items-center flex justify-between md:flex-nowrap flex-wrap md:px-10 px-4">
           {/* Brand */}
           <a
             className="text-white text-sm uppercase hidden lg:inline-block font-semibold"
