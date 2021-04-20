6f0152e17ad8a8859fa77493994da8dfadd411d5
Lukas <lukaskupczyk@users.noreply.github.com>
2021-04-20 09:53:35 +0200

    Update index.js
    fix typo

--- Landing Page/react-landing-page/src/index.js
+++ Landing Page/react-landing-page/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
-import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@fontawesome/fontawesome-free/css/all.min.css";
 
 import Landing from "views/Landing.js";
 
